5b5d873f5e7be29d673c992c4e6cd52cb5c13b0a
toapuro <164835903+toapuro@users.noreply.github.com>
2026-01-05 12:14:32 +0900

    AddonAPIを一旦削除

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,17 @@ plugins {
 
 publishing {
     publications {
-        create<MavenPublication>("addonapi_2.0.0") {
-            groupId = "moffy.addonapi"
-            artifactId = "addonapi"
-            version = "mc1.20.1-2.0.0-SNAPSHOT"
-            artifact(file("artifacts/addonapi/addonapi-mc1.20.1-2.0.0-SNAPSHOT.jar"))
-        }
+//        create<MavenPublication>("addonapi_2.0.0") {
+//            groupId = "moffy.addonapi"
+//            artifactId = "addonapi"
+//            version = "mc1.20.1-2.0.0-SNAPSHOT"
+//            artifact(file("artifacts/addonapi/addonapi-mc1.20.1-2.0.0-SNAPSHOT.jar"))
+//        }
     }
 
     repositories {
         maven {
-            url = buildDir.resolve("repo").toURI()
+            url = layout.buildDirectory.file("repo").get().asFile.toURI()
         }
     }
 }
