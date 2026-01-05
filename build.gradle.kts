13ce56392a0855c8d9382a4a5e3db2f131ea71e0
toapuro <164835903+toapuro@users.noreply.github.com>
2026-01-05 11:36:52 +0900

    バージョン修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ publishing {
         create<MavenPublication>("addonapi_2.0.0") {
             groupId = "moffy.addonapi"
             artifactId = "addonapi"
-            version = "2.0.0"
+            version = "mc1.20.1-2.0.0-SNAPSHOT"
             artifact(file("artifacts/addonapi/addonapi-mc1.20.1-2.0.0-SNAPSHOT.jar"))
         }
     }
